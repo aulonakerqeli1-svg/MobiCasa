612498f26fb3040dc2be0e2defa2f753202961c8
Aulona Kerqeli <aulonakerqeli1@gmail.com>
2025-11-26 19:10:08 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    const menuIcon = document.getElementById('menu-icon');
+    const navbar = document.getElementById('navbar');
+
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    });
+
